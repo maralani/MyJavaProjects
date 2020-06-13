665f9155fc081d90b454bcb27eebbf0d6cd74d15
Reza Maralani <maralani.turk@gmail.com>
2020-06-13 22:49:32 +0430

    Project001: Hello World.

---
+++ JavaProject/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.print("Welcome To Java Programming.\tOK!\n");
+
+    }//End of main
+}//End of class
