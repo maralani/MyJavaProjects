2a1bda1cb4db63f992dc91b74fef223c537e005b
Reza Maralani <maralani.turk@gmail.com>
2020-06-15 13:33:49 +0430

    Print a Number

--- JavaProject/src/com/company/Main.java
+++ JavaProject/src/com/company/Main.java
@@ -4,8 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.print("Welcome To Java Programming.\tOK!\n");
+        int N = 10 + 5;
+        System.out.format("N = %d\n", N);
+        System.out.print("N = " + N + "\n" );
+        System.out.println("-------------------");
+
+        N = 12345678;
+        System.out.format("N = %+,d", N);
+        System.out.println();
+        System.out.println("-------------------");
+
+        double pi = Math.PI;
+        System.out.format("Pi = %.2f", pi);
+
 
     }//End of main
 }//End of class
