b8aa47cf6cde06449cb72c0b37a978048b497ec6
Reza Maralani <maralani.turk@gmail.com>
2020-06-20 20:21:09 +0430

    The Divisors of a number

--- JavaProject/src/com/company/Main.java
+++ JavaProject/src/com/company/Main.java
@@ -10,29 +10,23 @@ public class Main {
         System.out.print("Please Enter a number\tn = ");
         int n = input.nextInt();
 
-        System.out.print("Please Enter the power of the number\tp = ");
-        int p = input.nextInt();
+        String result = divisors(n);
+        System.out.println(result);
 
-        int result = pow(n , p);
-        System.out.format("%d^%d = %d", n , p , result);
-
-        System.out.println();
-        System.out.println("---------------------------");
-
-        float m = (float) n;
-        System.out.println("Using Math.pow");
-        System.out.format("%.0f^%d = %.0f", m , p , Math.pow(m , p));
 
     }//End of main
-//----------------  Method: The power of a number  ----------------------
-    private static int pow (int n , int p){
+//----------------  Method: The Divisors of a number  ----------------------
+    private static String divisors (int n){
 
-        int r = 1;
+        String srt = "1";
 
-        for (int i =1; i <= p; i++){
-            r = r * n;
+        for (int i = 2; i <= n; i++){
+            if ( n % i == 0){
+                srt = srt + " , " + i;
+            }
 
         }
-        return r;
+
+        return srt;
     }
 }//End of class
