8f26037e5c206e843000a95e9bb2606b9500369f
Reza Maralani <maralani.turk@gmail.com>
2020-06-21 12:49:56 +0430

    Greatest Common Divisor and Least Common Multiple

--- JavaProject/src/com/company/Main.java
+++ JavaProject/src/com/company/Main.java
@@ -7,29 +7,62 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Pleas Enter A Number\t n = ");
-        float n = input.nextFloat();
+        System.out.print("Pleas Enter A Number\t n1 = ");
+        int n1 = input.nextInt();
 
-        System.out.print("Pleas Enter A Number (n / d)\t d = ");
-        int d = input.nextInt();
+        System.out.print("Pleas Enter A Number\t n2 = ");
+        int n2 = input.nextInt();
 
-        float temp = n;
+        int result = gcd(n1 , n2);
+        System.out.format("Greatest Common Divisor of %d and %d is:\t%d", n1, n2, result);
 
-        System.out.println("-------------- While loop ---------------");
+        System.out.println("\n------------------------------------------------------------");
 
-        while (n > d){
-            System.out.format("%.2f\n", n);
-            n = n /d;
-        }
+        result = lcm(n1 , n2);
+        System.out.format("Least Common Multiple of %d and %d is:\t%d", n1, n2, result);
 
-        System.out.println("------------ do_while loop --------------");
-
-        n = temp;
-
-        do {
-            System.out.format("%.2f\n", n);
-            n = n /d;
-        } while ( n > d);
+        System.out.println();
 
     }//End of main
+//----------------------- Method: Greatest Common Divisor ----------------------------
+    private static int gcd(int n1 , int n2){
+        int r = 0;
+
+        for (int i = 1; i <= n1; i++) {
+
+            if ( n1 % i == 0 && n2 % i == 0) r = i;
+
+        }
+
+        return r;
+    }
+
+//----------------------- Method: Least Common Multiple ------------------------------
+    private static int lcm(int n1 , int n2){
+        int r = 0;
+
+        int i = 2;
+
+        while (true) {
+
+            if ( n1 % i == 0 || i % n1 == 0) {
+
+                if (n2 % i == 0 || i % n2 == 0) {
+
+                    if (i >= n1 && i >= n2) {
+                        r = i;
+                        break;
+                    }
+
+                }
+
+            }
+
+            i++;
+
+        }
+
+        return r;
+    }
+
 }//End of class
