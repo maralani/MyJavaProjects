00a9f186871497f3a3c5a12f65ca5b3ef8b15bd9
Reza Maralani <maralani.turk@gmail.com>
2020-06-21 19:20:36 +0430

    Sine and Cosine

--- JavaProject/src/com/company/Main.java
+++ JavaProject/src/com/company/Main.java
@@ -7,57 +7,87 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Pleas Enter A Number\t n = ");
-        int n = input.nextInt();
 
-        String result = pascalTriangle(n);
-        System.out.println(result);
+        printMenu();
+
+        System.out.print("Select the item: ");
+        String str = input.nextLine();
+
+        System.out.format("%s ", str);
+        int d = input.nextInt();
+
+        double result = 0;
+
+        switch (str){
+            case "sin":
+                result = sin(d);
+                break;
+            case "cos":
+                result = cos(d);
+            default:
+        }
+
+        System.out.format("%s %d = %.2f", str, d, result);
+
+
 
     }//End of main
-//----------------------- Method: Pascal triangle ----------------------------
-    private static String pascalTriangle(int n){
-        String str = "";
+//----------------------- Method: Menu ----------------------------
 
-        for (int i = 1; i <=n; i++) {
-
-            str = str + space(n - i);
-            long fact = 1;
-            fact = fact(i);
-
-            for (int j = 0; j <= i; j++) {
-
-                int temp = 0;
-                long num = 0;
-                long factj = fact(j);
-                temp = i - j;
-                long factTemp = fact(temp);
-
-                num = fact / (factj * factTemp);
-
-                str = str + num + " ";
-
-            }//End of for(j)
-
-            str = str + space(n - i - 1);
-            str = str + "\n";
-        }
-
-        return str;
+    private static void printMenu(){
+        System.out.println("Trigonometric functions: Sin\tCos");
     }
 
-//----------------------- Method: Space ----------------------------
+//----------------------- Method: Sine ----------------------------
 
-    private static String space(int n){
-        String str = "";
+    private static double sin(int d){
 
-        for (int i = 1; i <=n; i++){
-            str = str + " ";
-        }
+        d = 180 / d;
+        double x = Math.PI / d;
 
-        return str;
+        double r = x;
+
+        int i = 3;
+        double Condition = 0;
+        int sign = -1;
+
+        do {
+            long fact = fact(i);
+            r = (float) (r + sign * (Math.pow(x, i) / fact));
+            i+=2;
+            Condition = Math.pow(x, i) / fact;
+            sign = sign * (-1);
+
+        } while ( Condition > .01);
+
+        return r;
     }
+
+//----------------------- Method: Cosine ----------------------------
+
+    private static float cos(int d){
+
+        d = 180 / d;
+        double x = Math.PI / d;
+
+        float r = 1;
+        int i = 2;
+        double Condition = 0;
+        int sign = -1;
+
+        do {
+            long fact = fact(i);
+            r = (float) (r + sign * (Math.pow(x, i) / fact));
+            i+=2;
+            Condition = Math.pow(x, i) / fact;
+            sign = sign * (-1);
+
+        } while ( Condition > .01);
+
+        return r;
+    }
+
 //----------------  Method: Factorial  ----------------------
-
     private static long fact(int n){
         long r = 1;
 
