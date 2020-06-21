ea2f9939d4a356aa2dcb1d4b2060b407f9de11d0
Reza Maralani <maralani.turk@gmail.com>
2020-06-21 13:19:17 +0430

    Fibonacci Number

--- JavaProject/src/com/company/Main.java
+++ JavaProject/src/com/company/Main.java
@@ -10,22 +10,38 @@ public class Main {
         System.out.print("Pleas Enter A Number\t n = ");
         int n = input.nextInt();
 
-        int result = anagram(n);
-        System.out.format("Anagram of %d = %d", n , result);
-
+        String result = fib(n);
+        System.out.println(result);
 
     }//End of main
-//----------------------- Method: Anagram of a number ----------------------------
-    private static int anagram (int n){
-        int r = 0;
+//----------------------- Method: Fibonacci Number ----------------------------
+    private static String fib(int n){
+        String srt = "";
 
-        while ( n > 0) {
+        long temp1 = 0; //Fib1
+        long temp2 = 1; //Fib2
+        long fib = 1;
+        int i = 3;
 
-            r = ( n % 10 ) + (r * 10);
-            n = n / 10;
+        while (true) {
 
-        }
+            fib = fib + temp1;
+            temp1 = temp2;
+            temp2 = fib;
 
-        return r;
+            if ( n == fib ) {
+                srt = i + "th number of fibonacci number is " + n + ".";
+                break;
+            }else if ( n < fib ) {
+                srt = n + " is not a fibonacci number!";
+                break;
+            } else {
+                i++;
+            }
+
+        }//End of while
+
+        return srt;
     }
+
 }//End of class
