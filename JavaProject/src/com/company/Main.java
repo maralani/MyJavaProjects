ade8282e6cfeb336088fbb1a8fc83c5c1ef8b301
Reza Maralani <maralani.turk@gmail.com>
2020-06-24 20:25:36 +0430

    Sub String

--- JavaProject/src/com/company/Main.java
+++ JavaProject/src/com/company/Main.java
@@ -10,27 +10,27 @@ public class Main {
         System.out.print("Please Enter A String: str = ");
         String str = input.nextLine();
 
-        System.out.print("Please Enter A Number n = ");
-        int n = input.nextInt();
+        System.out.print("Please Enter A Number i = ");
+        int i = input.nextInt();
+        System.out.print("Please Enter A Number l = ");
+        int l = input.nextInt();
 
         String result = "";
-        result = setW(str, n);
+        result = subString(str, i, l);
         System.out.println(result);
 
 
     }//End of main
 
-//--------------------------- String: setW ------------------------------
+//--------------------------- String: Sub String ------------------------------
 
-    private static String setW(String s, int n){
+    private static String subString(String s, int in, int l){
 
         String str = "";
 
-        for (int i = 0; i < n - s.length() ; i++) {
-            str= str + ' ';
+        for (int i = in-1; i < in + l - 1 ; i++) {
+            str = str + s.charAt(i);
         }
-
-        str = str + s;
 
         return str;
     }
