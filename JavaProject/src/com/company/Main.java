60b83b1a11a094f9fa41250d7e2c854cbc60af6f
Reza Maralani <maralani.turk@gmail.com>
2020-06-24 19:08:58 +0430

    String: Delete digits

--- JavaProject/src/com/company/Main.java
+++ JavaProject/src/com/company/Main.java
@@ -1,13 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 256; i++) {
-            char c = (char) i;
-            System.out.format("%d = %c\n", i , c);
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter A String: str = ");
+        String str = input.nextLine();
+
+        String result = "";
+        result = deleteDigits(str);
+        System.out.println(result);
 
     }//End of main
+
+//--------------------------- Delete digits ------------------------------
+
+    private static String deleteDigits(String s){
+
+        String str = "";
+        for (int i = 0; i <s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                continue;
+            }
+            str = str + s.charAt(i);
+        }
+
+        return str;
+    }
+
 }//End of class
